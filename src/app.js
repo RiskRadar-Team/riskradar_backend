b28ccdc938a9b7a4c2eeabb61cbe5dc77e0f9702
Sandeep Kumar <sksandeepds37@gmail.com>
2026-08-13 14:37:41 +0530

    Fixed: cors error for deployed frontend

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://riskradar-b6ne97etr-sahitya28s-projects.vercel.app/",
+  "https://riskradar-b6ne97etr-sahitya28s-projects.vercel.app",
 ];
 
 app.use(
